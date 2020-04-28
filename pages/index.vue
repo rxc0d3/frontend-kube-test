733315a954aeb7e6aa97f017a60456cffe220d88
raul <rlopezaldana@gmail.com>
2020-04-28 18:08:23 -0500

    config server axios

--- pages/index.vue
+++ pages/index.vue
@@ -7,10 +7,17 @@
       </h2>
       <div class="links">
         <a
+          @click.prevent="loadData"
+          href="#"
+          target="_blank"
+          class="button--green">Load Data Service Normal</a>
+      </div>
+      <div class="links">
+        <a
           @click.prevent="loadDataTesting"
           href="#"
           target="_blank"
-          class="button--green">Load Data Service</a>
+          class="button--green">Load Data Service Witch Server Express</a>
       </div>
       <div style="padding-top:1em">
         <h4>result service :</h4>
@@ -39,10 +46,7 @@ export default {
   },
   methods:{
     async loadData() {
-   
-      console.log(this.$env);
       let url = `${this.$env.SVC_URL}/api/v1/clients`;
-      // let url = `http://localhost:5000/api/v1/clients`;
       try {
           this.response = await this.$axios.$get(url);
       } catch(err) {
@@ -52,11 +56,9 @@ export default {
       }
     },
     async loadDataTesting() {
-  
       let url = `http://157.230.199.26/api/tests`;
       try {
-          let response = await this.$axios.$get(url);
-          console.log(response);
+          this.response = await this.$axios.$get(url);
       } catch(err) {
           console.log(err.response);
       }
